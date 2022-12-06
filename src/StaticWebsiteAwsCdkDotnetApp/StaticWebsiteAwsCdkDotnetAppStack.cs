07a2b3ec4fd597296c403b367b9ebfd7f331302e
omerkarakas <karakasomer@gmail.com>
2022-12-06 17:52:42 +0300

    Initial commit

---
+++ src/StaticWebsiteAwsCdkDotnetApp/StaticWebsiteAwsCdkDotnetAppStack.cs
@@ -0,0 +1,13 @@
+using Amazon.CDK;
+using Constructs;
+
+namespace StaticWebsiteAwsCdkDotnetApp
+{
+    public class StaticWebsiteAwsCdkDotnetAppStack : Stack
+    {
+        internal StaticWebsiteAwsCdkDotnetAppStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
+        {
+            // The code that defines your stack goes here
+        }
+    }
+}
