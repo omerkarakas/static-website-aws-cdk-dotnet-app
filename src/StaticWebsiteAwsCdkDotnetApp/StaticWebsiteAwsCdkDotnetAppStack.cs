d22e1be032633a6fed994fc2ed5218489b6225d5
omerkarakas <karakasomer@gmail.com>
2022-12-07 09:23:03 +0300

    static website by cdk

--- src/StaticWebsiteAwsCdkDotnetApp/StaticWebsiteAwsCdkDotnetAppStack.cs
+++ src/StaticWebsiteAwsCdkDotnetApp/StaticWebsiteAwsCdkDotnetAppStack.cs
@@ -1,5 +1,9 @@
 using Amazon.CDK;
-using Constructs;
+using Amazon.CDK.AWS.IAM;
+using Amazon.CDK.AWS.S3;
+using Amazon.CDK.AWS.S3.Deployment;
+//using Constructs;
+using System.Diagnostics;
 
 namespace StaticWebsiteAwsCdkDotnetApp
 {
@@ -8,6 +12,38 @@ namespace StaticWebsiteAwsCdkDotnetApp
         internal StaticWebsiteAwsCdkDotnetAppStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
         {
             // The code that defines your stack goes here
+            var bucket = new Bucket(this, "bucket", new BucketProps
+            {
+                BucketName = "okcdkdemo",
+                AccessControl = BucketAccessControl.PUBLIC_READ,
+                WebsiteIndexDocument = "index.html"
+            });
+
+            var policyStatement = new PolicyStatement(new PolicyStatementProps
+            {
+                Effect = Effect.ALLOW,
+                Actions = new[] { "s3:GetObject" },
+                Resources = new[] { bucket.ArnForObjects("*") },
+                Principals = new IPrincipal[]
+                {
+                    new AnyPrincipal()
+
+                }
+
+            });
+
+            bucket.AddToResourcePolicy(policyStatement);
+
+            new BucketDeployment(this, "mydeployment", new BucketDeploymentProps
+            {
+                DestinationBucket = bucket,
+                Sources = new[] {Source.Asset("./site-contents")}
+            });
+
+            new CfnOutput(this, "bucketurl", new CfnOutputProps
+            {
+                Value = bucket.BucketWebsiteUrl
+            });
         }
     }
 }
